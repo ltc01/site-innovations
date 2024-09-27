30c2fe80678ffe7f00cdea69b0bfe5ef2743c731
Abhishek Rajput <abhishekrajput9111606916@gmail.com>
2024-09-28 01:56:54 +0530

    minor styling added read more button to cards

--- src/Components/School/Program.jsx
+++ src/Components/School/Program.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Keyboard, Navigation, Pagination, Scrollbar } from "swiper/modules";
+import { BiRightArrow } from "react-icons/bi";
+import { FaArrowRightArrowLeft, FaChevronRight } from "react-icons/fa6";
 
 // Register ScrollTrigger with GSAP
 gsap.registerPlugin(ScrollTrigger);
 
 const Program = ({ title, data }) => {
+  const navigate = useNavigate();
   const titleRef = useRef(null);
   const buttonRefs = useRef([]);
   console.log(data);
@@ -75,92 +78,7 @@ const Program = ({ title, data }) => {
       >
         {title} Programs
       </h1>
-      {/* <div className="grid grid-cols-1 md:grid-cols-2 justify-center mt-8 gap-2 md:gap-4">
-        {data?.map((course, index) => (
-          <Link to={`/course/${course.title}/${course.id}`}
-            key={index}
-            ref={(el) => (buttonRefs.current[index] = el)}
-            className="dark:bg-indigo-600 w-full px-4 py-2 md:px-6 md:py-2 border border-none bg-slate-100 rounded-md text-sm md:text-base"
-          >
-            <span className="relative z-10 flex items-center gap-4 ">
-              <span className="">{course.title}</span>
-            </span>
-          </Link>
-        ))}
-      </div> */}
-      {/* <div className="mx-4 mt-8 md:mx-14">
-        <Swiper
-          modules={[Navigation]}
-          spaceBetween={0} // Adjust the space between cards
-          slidesPerView={1} // Default number of slides per view
-          dot={false}
-          // loop={true} // Enables loop mode for continuous sliding
-          navigation={{
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-          }}
-          pagination={{ clickable: true }}
-          className="swiper-container lg:px-6"
-          onSlideChange={handleSlideChange}
-          breakpoints={{
-            320: {
-              slidesPerView: 1,
-              spaceBetween: 10,
-            },
-            620: {
-              slidesPerView: 2,
-              spaceBetween: 10,
-            },
-            740: {
-              slidesPerView: 3,
-              spaceBetween: 20,
-            },
-            1024: {
-              slidesPerView: 4,
-              spaceBetween: 10,
-            },
-          }}
-        >
-          {data?.map((course, index) => (
-            <SwiperSlide key={index}>
-              <div className="h-[22.5rem] overflow-hidden dark:bg-indigo-900 dark:border shadow-md rounded-xl m-2">
-                <div className="relative h-[50%]">
-                  <img
-                    src={''}
-                    alt={course.title}
-                    className="w-full h-full object-cover"
-                  />
-
-                  <div className="absolute top-3 left-4 flex">
-
-                    <span className="text-xs mr-3 bg-gradient-to-r from-orange-600 to-amber-500 text-white rounded-lg px-3 py-1">
-                      Premium
-                    </span>
-
-                    <span className="bg-slate-200 text-gray-800 rounded-lg text-xs px-3 py-1">
-                      Plus
-                    </span>
-                  </div>
-                </div>
-                <div className="pl-4 h-[45%] flex flex-col justify-between">
-                  <div className="">
-                    <h3 className="text-xl font-semibold my-2 text-nowrap">{course.title} </h3>
-                    <p className="text-sm pr-3 text-slate-600">{course.description}</p>
-                  </div>
-                  <button
-                    // onClick={() => navigate(`/course/${course.title}/${course.id}`)}
-                    className="bg-gradient-to-r w-fit mb-4 mt-1 rounded-md text-xs from-indigo-700 to-indigo-400 text-white px-4 py-2 font-semibold hover:bg-gradient-to-l transition-all ease-in-out duration-300"
-                  >
-                    View More
-                  </button>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div> */}
-
-<div className="">
+      <div className="">
         <Swiper
           keyboard={{ enabled: true }}
           navigation={true}
@@ -173,49 +91,56 @@ const Program = ({ title, data }) => {
             768: { slidesPerView: 3 },
             1024: { slidesPerView: 3.5 },
             1200: { slidesPerView: 4 },
-          }}  
+          }}
           onSlideChange={() => console.log("Slide changed")}
           onSwiper={(swiper) => console.log("Swiper initialized")}
           className="md:h-[350px] w-full py-4 m-auto flex flex-col justify-center items-center gap-4"
         >
           {data?.map((course, index) => {
-           
+
             return (
               <SwiperSlide key={index}>
-              <div className="h-80 w-fit min-w-56 my-4 bg-gradient-to-r from-teal-50  to-indigo-100  shadow-lg overflow-hidden dark:from-indigo-950  dark:to-teal-700 dark:border rounded-xl m-2">
-                <div className="relative h-[50%]">
-                  <img
-                    src={'https://miro.medium.com/v2/resize:fit:720/1*aBQrwweY6-qFVWeizUrTnQ.png'}
-                    alt={course.title}
-                    className="w-full h-full object-cover"
-                  />
-                  <div className="absolute bottom-3 left-4 flex">
+                <div className="h-80 w-fit min-w-56 my-4 bg-gradient-to-r from-teal-50  to-indigo-100 shadow-lg overflow-hidden dark:from-indigo-950  dark:to-teal-700  mx-auto dark:border rounded-xl m-2" >
+                  <div className="relative h-[50%]">
+                    <img
+                      src={'https://miro.medium.com/v2/resize:fit:720/1*aBQrwweY6-qFVWeizUrTnQ.png'}
+                      alt={course.title}
+                      className="w-full h-full object-cover"
+                    />
+                    <div className="absolute bottom-3 left-4 flex">
 
-                    <span className="text-xs mr-3 flex-1 bg-gradient-to-r from-orange-600 to-amber-500 text-white rounded-lg px-3 py-1">
-                      Premium
-                    </span>
+                      <span className="text-xs mr-3 flex-1 bg-gradient-to-r from-orange-600 to-amber-500 text-white rounded-lg px-3 py-1">
+                        Premium
+                      </span>
 
-                    <span  className="bg-slate-200 flex-1 text-gray-800 rounded-lg text-xs px-3 py-1">
-                      Plus
-                    </span>
+                      <span className="bg-slate-200 flex-1 text-gray-800 rounded-lg text-xs px-3 py-1">
+                        Plus
+                      </span>
+                    </div>
+                  </div>
+                  <div className="px-4 h-[50%] gap-8 flex flex-col justify-between">
+                    <div className="">
+                      <h3 className="text-xl font-semibold my-2 text-wrap">{course.title.length <= 20 ? course.title : course.title.slice(0, 20) + '...'} </h3>
+                      {/* <p className="text-sm pr-3 text-slate-600 text-justify">{course.description}</p> */}
+                    </div>
+                    <div className="flex items-center my-3 gap-4 text-nowrap justify-between w-full ">
+                      <button
+                        // onClick={() => navigate(`/course/${course.title}/${course.id}`)}
+                        className="text-lg font-bold w-fit rounded-md"
+                      >
+                        ₹ {course.plans[0].price}
+                      </button>
+                      <button
+                        onClick={() => navigate(`/course/${course.title}/${course.id}`)}
+                        className="bg-gradient-to-r text-base from-indigo-700 to-indigo-400 text-white px-3 py-1 font-semibold hover:bg-gradient-to-l transition-all ease-in-out duration-300 flex items-center justify-center w-fit rounded-md"
+                      >
+                        <p>Read More</p>
+                        <FaChevronRight size={12} />
+                      </button>
+                    </div>
                   </div>
                 </div>
-                <div className="px-4 flex flex-col justify-evenly">
-                  <div className="">
-                    <h3 className="text-xl font-semibold my-2 text-wrap">{course.title.length <= 20 ?course.title : course.title.slice(0,20)+'...'} </h3>
-                    {/* <p className="text-sm pr-3 text-slate-600 text-justify">{course.description}</p> */}
-                  </div>
-                 {/* <div className="relative h-full"> */}
-                 <button
-                    // onClick={() => navigate(`/course/${course.title}/${course.id}`)}
-                    className="text-lg font-bold absolute bottom-2 w-fit rounded-md"
-                  >
-                    ₹ {course.plans[0].price}
-                  </button>
-                 {/* </div> */}
-                </div>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
             )
           })}
         </Swiper>
