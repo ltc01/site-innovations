2c324779f918325a5e69de17d4efd159762fea22
Ajay Goud <ajaygoud059@gmail.com>
2024-09-27 15:45:09 +0530

    added gsap for aboutus,gcep,ourteam.

--- src/Components/GSCB/GSCB-Hero.jsx
+++ src/Components/GSCB/GSCB-Hero.jsx
@@ -1,56 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import demo from "../../assets/GSCP/favicon.mp4";
 
 const HeroSection = () => {
+
+  
+
   useEffect(() => {
-    const sentences = gsap.utils.toArray(".sentence");
 
-    const tl = gsap.timeline({ repeat: -1 });
+    const tl = gsap.timeline()
 
-    sentences.forEach((sentence) => {
-      const h1s = sentence.querySelectorAll("h1");
-      const h2s = sentence.querySelectorAll("h2");
-      const spans = sentence.querySelectorAll(".spn");
+    tl.fromTo('.anime1',{opacity:0,y:30},{
+      opacity:1,
+      duration:1,
+      y:0,
+      stagger:0.2,
+      ease:'back.inOut',
+    })
 
-      tl.fromTo(sentence, { display: "none" }, { display: "flex", duration: 0 })
-        .fromTo(
-          spans,
-          { scaleY: 0, transformOrigin: "center" },
-          {
-            scaleY: 1,
-            opacity: 1,
-            duration: 1,
-            ease: "back.inOut",
-            stagger: 0.3,
-          }
-        )
-        .fromTo(
-          h1s,
-          { opacity: 0, x: 50 },
-          {
-            opacity: 1,
-            x: 0,
-            duration: 1,
-            ease: "back.inOut",
-            stagger: 0.3,
-          }
-        )
-        .fromTo(
-          h2s,
-          { opacity: 0, x: -35 },
-          {
-            opacity: 1,
-            x: 0,
-            duration: 1,
-            ease: "back.inOut",
-            stagger: 0.3,
-          },
-          "-=0"
-        )
+    tl.fromTo('.anime3',{transformOrigin:'center',scaleY:0},{
+      opacity:1,
+      duration:1,
+      scaleY:1,
+      stagger:0.2,
+      ease:'back.inOut',
+    },
+    '-=0.5'
+  )
 
-        .to(sentence, { display: "none", duration: 2 });
-    });
+    tl.fromTo('.anime2',{opacity:0,y:30},{
+      opacity:1,
+      duration:1,
+      y:0,
+      stagger:0.2,
+      ease:'back.inOut',
+    },
+    '-=0.9'
+  )
+
   }, []);
 
   return (
@@ -67,17 +54,23 @@ const HeroSection = () => {
         Your browser does not support HTML5 video.
       </video>
   
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-  
-      <div className="text-white font-extrabold flex  flex-col items-start md:items-center justify-center w-full h-full relative text-center">
-        <div className="sentence  gap-4 w-full flex flex-col items-start md:flex-row md:items-center text-center absolute md:w-[65%] justify-center h-full mx-auto px-10 top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] max-xs:gap-1">
-          <h2 className="bg-gradient-to-r from-pink-500  to-violet-600  text-transparent hero py-3">
-            <span className="text-4xl px-4 md:text-4xl text-white lg:text-5xl">GCEP</span>
-          </h2>
-          <span className="w-1 h-36 bg-white hidden md:block max- xs:h-24"></span>
-          <p className="text-xl hero-tail text-left md:text-4xl xl:text-5xl">Global Collabo <span className="text-nowrap">Educational Partnership</span></p>
-        
+      {/* <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div> */}
+
+      <div className="absolute top-0 left-0 pointer-events-none z-2 inset-0">
+          <div className="w-full h-full bg-black opacity-65"></div>
         </div>
+  
+      <div className="text-white font-extrabold flex  flex-col items-start md:items-center justify-center w-full h-full relative text-center z-3 inset-0 ">
+        <div className="sentence  gap-4 w-full flex flex-col items-start md:flex-row md:items-center text-center absolute md:w-[65%] justify-center h-full mx-auto px-10 top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] max-xs:gap-1">
+          <h2 className="anime1 bg-gradient-to-r from-pink-500  to-violet-600  text-transparent hero md:py-3 p-1">
+            <span className="anime1 text-xl px-4 md:text-4xl text-white lg:text-5xl">GCEP</span>
+          </h2>
+          <span className="anime3 w-[1vw] h-[8vw] hidden md:block bg-white">.</span>
+
+          <div className="text-xl  hero-tail text-left md:text-4xl xl:text-5xl ">
+            <h2 className="anime2">Global Collabo </h2>  
+            <h2 className="text-nowrap anime2 ">Educational Partnership </h2> </div> 
+      </div>
       </div>
     </div>   
   </section>
